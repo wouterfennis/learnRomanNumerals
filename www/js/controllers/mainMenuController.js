06648d00b6714937185be8a390831d0124d8f191
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-08-25 12:13:12 +0200

    test

--- www/js/controllers/mainMenuController.js
+++
@@ -1,5 +0,0 @@
-angular.module('app.controllers', [])
-
-  .controller('MainMenuController', function($scope) {
-    console.log("Main Menu Controller");
-  });
--- www/js/controllers/romanToDecimalController.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.controllers')
-
-  .controller('RomanToDecimalController', function($scope, RomanNumeralsService) {
-    console.log("Roman To Decimal controller");
-    console.log(RomanNumeralsService.calculateRomanNumeralToDecimal("VIII"));
-  });
