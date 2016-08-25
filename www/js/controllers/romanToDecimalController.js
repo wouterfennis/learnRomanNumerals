e8532ed2624518c1761b9ae172aff3ebc24d2916
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-08-25 16:04:03 +0200

    ADDED controller logic to handle user input

--- www/js/controllers/romanToDecimalController.js
+++ www/js/controllers/romanToDecimalController.js
@@ -5,4 +5,34 @@ angular.module('app.controllers')
       $scope.decimalAnswer = "";
 
 
+      $scope.addCharToAnswer = function(enteredChar){
+        $scope.decimalAnswer = $scope.decimalAnswer + enteredChar;
+      };
+
+      $scope.removeLastCharFromAnswer = function(){
+        var lastCharPosition = -1;
+        $scope.decimalAnswer = $scope.decimalAnswer.slice(0, lastCharPosition);
+      };
+
+      $scope.clearDecimalAnswer = function(){
+        $scope.decimalAnswer = "";
+      };
+
+      $scope.checkAnswer = function(){
+        var correctAnswer = RomanNumeralsService.calculateRomanNumeralToDecimal($scope.romanNumeral);
+        var userAnswer = parseInt($scope.decimalAnswer);
+        if(correctAnswer === userAnswer){
+          showSuccessMessage("Correct Answer!");
+        } else {
+          showErrorMessage("Wrong Answer!");
+        }
+      };
+
+      function showSuccessMessage(message){
+        alert(message);
+      }
+
+      function showErrorMessage(message){
+        alert(message);
+      }
   });
